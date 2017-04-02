0fbe0dbaabb8f33d4c3412325b3895e7e942d19d
David <davidnuno281@gmail.com>
2017-04-02 15:39:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/ufcapp/Fighter.java
@@ -0,0 +1,120 @@
+package com.example.android.ufcapp;
+
+/**
+ * This class is used to describe a current UFC fighter.
+ */
+
+public class Fighter {
+
+    /** The first name. */
+    private String firstName;
+
+    /** The last name. */
+    private String lastName;
+
+    /** The nickname name. */
+    private String nickname;
+
+    /** The age. */
+    private int age;
+
+    /** Number of wins. */
+    private int wins;
+
+    /** Number of losses. */
+    private int losses;
+
+    /** Number of draws. */
+    private int draws;
+
+    /** Is he/she champion is division. */
+    private boolean isChampion;
+
+    /**
+     * The constructor.
+     *
+     * @param firstName The first name of the fighter.
+     * @param theLast  The last name of the fighter.
+     * @param nickname The fighters nickname.
+     * @param wins  The number of wins of the fighter.
+     * @param losses  The number of losses of the fighter.
+     * @param draws The number of draws of the fighter.
+     * @param isChampion Is the fighter a current champion in his/her weight division.
+     * @param age The age of the fighter.
+     * */
+    public Fighter (String firstName, String theLast, String nickname, int wins, int losses, int draws, boolean isChampion, int age) {
+
+        this.firstName   = firstName;
+        this.lastName    = theLast;
+        this.nickname    = nickname;
+        this.wins        = wins;
+        this.losses      = losses;
+        this.draws       = draws;
+        this.isChampion  = isChampion;
+        this.age         = age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setChampion(boolean isChampion) {
+        this.isChampion = isChampion;
+    }
+
+    public boolean isChampion() {
+        return isChampion;
+    }
+}
